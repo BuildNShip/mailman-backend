70fbcd80ed4d280c8c4b3d8becdb10bd05fa06d3
shamila p <lulushamila@gmail.com>
2023-04-10 04:05:25 +0530

    updated url

--- mailman/urls.py
+++ mailman/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('mailman/v1/api/', include('api.urls')),
 ]
